52cffac4314f377c7d2389e4785378a3c9477d6d
Kamil Kwiatkowski <kamil.kwiatkowski2684@gmail.com>
2025-05-13 19:48:58 +0200

    walking animation added

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,20 +6,40 @@ public class PlayerMovement : MonoBehaviour
 {
 
     [SerializeField] private float moveSpeed = 5f;
-    private Rigidbody2D rb;
-    private Vector2 moveInput;
+    private Rigidbody2D _rb;
+    private Vector2 _moveInput;
+
+    private Animator _animator;
+
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        rb.linearVelocity = moveInput * moveSpeed;
+        _rb.linearVelocity = _moveInput * moveSpeed;
     }
 
     public void Move(InputAction.CallbackContext context)
     {
-        moveInput = context.ReadValue<Vector2>();
+        _moveInput = context.ReadValue<Vector2>();
+
+        if (_moveInput == Vector2.zero) {
+            _animator.Play("Idle");
+        }
+        if (_moveInput == Vector2.up) {
+            _animator.Play("WalkBack");
+        }
+        if (_moveInput == Vector2.down) {
+            _animator.Play("WalkFront");
+        }
+        if (_moveInput == Vector2.left) {
+            _animator.Play("WalkLeft");
+        }
+        if (_moveInput == Vector2.right) {
+            _animator.Play("WalkRight");
+        }
     }
 }
